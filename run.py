ad6664bb4bc7e2026573f6facff94e924b9f20eb
GregDeLaMort <gchalony@yahoo.fr>
2020-04-25 01:23:19 +0400

    Optimize imports

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-import json
 import logging
 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 from werkzeug.utils import redirect
 
 from codenameapp.utils import generate_random_words, parse_cell_code, generate_response_grid, genid
