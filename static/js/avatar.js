7ab35077fd668715e6f0544607d232a738088444
GregDeLaMort <gchalony@yahoo.fr>
2020-04-25 20:41:45 +0400

    Add avatar

---
+++ static/js/avatar.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById("av");
+var context = canvas.getContext('2d');
+var w = canvas.width, h = canvas.height;
+
+
+function drawAvatar(body, head, mouth){
+    context.beginPath();
+    context.strokeStyle = "#000000"
+    // Body
+    context.fillStyle = body;
+    context.arc(w/2, h/2+110, 70, 0, Math.PI * 2); // Ici le calcul est simplifié
+    context.fill();
+    context.stroke();
+
+
+    // Head
+    context.fillStyle = head;
+    context.arc(w/2, h/2, 50, 0, Math.PI * 2); // Ici le calcul est simplifié
+    context.fill();
+
+    context.beginPath(); // La bouche, un arc de cercle
+    context.fillStyle = mouth;
+    context.arc(w/2, h/2, 40, 0, Math.PI); // Ici aussi
+    context.fill();
+
+    context.beginPath(); // L'œil gauche
+    context.arc(w/2 - 20, h/2 - 5, 20, (Math.PI / 180) * 220, (Math.PI / 180) * 320);
+    context.stroke();
+
+    context.beginPath(); // L'œil droit
+    context.arc(w/2 + 20, h/2 - 5, 20, (Math.PI / 180) * 220, (Math.PI / 180) * 320);
+    context.stroke();
+}
+
+drawAvatar("#ff0", "#ff0", "#ff0000")
+
+backcol = document.getElementById("backcol");
+mouthcol = document.getElementById("mouthcol");
+
+backcol.addEventListener('change', function(e){
+    drawAvatar(e.target.value, e.target.value, mouthcol.value);
+});
+mouthcol.addEventListener('change', function(e){
+    drawAvatar(backcol.value, backcol.value, e.target.value);
+});
---
+++ static/js/room.js
@@ -0,0 +1,7 @@
+var go = document.getElementById("go");
+go.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('Go!')
+    // Sent some kind of request to the backend to be redirected to the grid
+
+})
