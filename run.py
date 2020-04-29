55673d7ad4a35470d89a9acd2a832807ef865251
GregDeLaMort <gchalony@yahoo.fr>
2020-04-29 12:45:56 +0400

    Cleanup and add comments

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ from codenameapp.game import Game, GameNamespace
 from codenameapp.room import RoomNamespace
 from codenameapp.utils import parse_cell_code, genid
 
+# Logging config (could move to config file)
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -20,15 +21,14 @@ logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 app = Flask(__name__)
 app.config.from_object(config)
-Session(app)
-socketio = SocketIO(app, manage_session=False)
+Session(app)  # Instanciate session to store data to filesystem
+socketio = SocketIO(app, manage_session=False)  # Let flask-session handle sessions
 
 
 g = Game(["greg", "clem", "sol", "axel"], [["greg", "clem"], ["sol", "axel"]])
 games = {"0": g}
 
 temp_default_teams = [["Greg", "Sol"], ["Axel", "Clem"]]
-
 
 
 @app.route("/index")
@@ -40,18 +40,27 @@ def get_home():
 @app.route("/new_room")
 def create_new_room():
     room_id = uuid4().hex
-    print(f"Created new room {room_id}")
-    pseudo = request.args.get("pseudo", None)
-    col1 = request.args.get("col1", "")
-    col2 = request.args.get("col2", "")
-    resp = redirect(f"{room_id}/room")
-    user_id = session.get("user_id", uuid4().hex)
+    # Should store this room id somewhere, and possibly the user who created it
 
-    expire_date = datetime.datetime.now() + datetime.timedelta(30)  # 30 days ahead
+    logger.debug(f"Created new room {room_id}")
+    pseudo = request.args.get("pseudo", None)
+    col1 = request.args.get("col1", None)
+    col2 = request.args.get("col2", None)
+
+    if pseudo is None or col1 is None or col2 is None:
+        return "Missing parameters", 400
+
+    user_id = session.get("user_id", uuid4().hex)  # Create new user_id if not already stored
     session["user_id"] = user_id
     session["pseudo"] = pseudo
     session["avatar-col1"] = col1
     session["avatar-col2"] = col2
+
+    # Here we should store the user in DB and in a dict or smth
+
+    resp = redirect(f"{room_id}/room")
+    # Add cookies (could attach them to the home page to avoid sending them all the time...)
+    expire_date = datetime.datetime.now() + datetime.timedelta(30)  # 30 days ahead
     resp.set_cookie("user_id", user_id, expires=expire_date)
     resp.set_cookie("pseudo", pseudo, expires=expire_date)
     resp.set_cookie("avatar-col1", col1, expires=expire_date)
@@ -66,36 +75,20 @@ def get_grid(room_id):
 
 @app.route("/<room_id>/room")
 def get_room(room_id):
-    print(session)
-    return render_template("room.html")
+    logger.debug(session)
+    return render_template("room.html", initial_player_pseudo=session.get("pseudo", None))
 
 
-@app.route("/<room_id>/cell")
-def get_cell_data(room_id):
-    cell_code = request.args.get("code")
-    r, c = parse_cell_code(cell_code)
-    val = games[room_id].answers[r, c]
-    logger.debug(f"Returning value for cell {cell_code} : {val}")
-    return str(val)
-
-
-### SOCKET CALLBACKS  ###
+### GENERAL SOCKET CALLBACKS  ###
 @socketio.on("connect")
 def handle_connect():
-    print(f"Socket {request.sid} connected!")
+    # Have access to session here
+    logger.debug(f"Socket {request.sid} (user {session.get('pseudo', None)}) connected!")
 
 
 @socketio.on("disconnect")
 def handle_disconnect():
-    print(f"Socket {request.sid} disconnected!")
-
-
-@socketio.on("create room")
-def handle_create_room(data):
-    room_id = genid()
-    join_room(room_id)
-    room_url = f"{room_id}/room"
-    emit("url redirection", {"url": room_url}, broadcast=True)
+    logger.debug(f"Socket {request.sid} (user {session.get('pseudo', None)}) disconnected!")
 
 
 socketio.on_namespace(RoomNamespace("/room", games.update))
--- static/js/room.js
+++ static/js/room.js
@@ -1,4 +1,9 @@
-var socket = io("/room");
+var socket = io("/room");  // Connect to namespace /room
+
+var home = document.getElementById("home");
+home.addEventListener("click", function(){
+    window.location = "/";
+});
 
 var go = document.getElementById("go");
 go.addEventListener("click", function(e){
@@ -6,11 +11,6 @@ go.addEventListener("click", function(e){
     console.log("Go!");
     console.log(socket);
     socket.emit("start_game");
-});
-
-var home = document.getElementById("home");
-home.addEventListener("click", function(){
-    window.location = "/";
 });
 
 socket.on("url_redirection", function(data){
