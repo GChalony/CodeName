53d6f3ae4d77771fe1c4c30051e0daabfe23be46
GregDeLaMort <gchalony@yahoo.fr>
2020-05-03 16:51:25 +0400

    Add pseudo to User

--- server/users.py
+++ server/users.py
@@ -1,7 +1,7 @@
 class User():
-    def __init__(self, user_id):
+    def __init__(self, user_id, pseudo=""):
         self.id = user_id
-        self.nickname = ""
+        self.pseudo = pseudo
 
     def __repr__(self):
-        return f"<{self.id}>"
+        return f"<nickname={self.pseudo}>"
