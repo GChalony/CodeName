1837935f6719471dd232d7af2d6a45cb34139795
GregDeLaMort <gchalony@yahoo.fr>
2020-07-24 18:56:31 +0400

    Sanitize words :sunglasses:

--- scripts/words_utils.py
+++ scripts/words_utils.py
@@ -1,5 +1,6 @@
 # This script contains a few utils method, meant to be called from the console
 # to handle words, dropping duplicates etc...
+import sys
 
 WORDS_PATH = "static/ressources/words.csv"
 
@@ -31,3 +32,29 @@ def normalize():
     capitalized = [w.capitalize() for w in words]
     with open(WORDS_PATH, "w", encoding='utf8') as f:
         f.writelines(capitalized)
+
+
+def _get_code(char):
+    return int.from_bytes(char.encode(), sys.byteorder)
+
+
+chars_levels = {
+    "warn": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "classic": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ \néèêëàâîôûçÂ-'"
+}
+char_levels_codes = {
+    level: [_get_code(c) for c in s] for level, s in chars_levels.items()
+}
+
+
+def get_weird_chars(words=None, level="classic"):
+    if words is None:
+        words = read_words()
+    res = []
+    for l, w in enumerate(words):
+        for c in w:
+            code = _get_code(c)
+            if code not in char_levels_codes[level]:
+                print(l, c, w)
+                res.append((l, c))
+    return res
